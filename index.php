1288e5a341b52d04999c92765aaf61668ec5441b
sparkakapil <sparkakapil@gmail.com>
2018-04-08 03:52:48 +0530

    Layout of WebPage Ready

---
+++ index.php
@@ -0,0 +1,194 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>CHETAN</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+
+	<!-- REQUIRED FOR SYMBOL USED FOR SEARCH BUTTON  -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+
+<body>
+
+
+<!-- NAVBAR CODE -->
+
+<div class="topnav" id="myTopnav">
+
+  <a href="#home" class="active">Home</a>
+  
+  <a href="#news">News</a>
+  <a href="#contact">Contact</a>
+  <a href="#about">About</a>
+  	<span class="SearchBarInNav">
+		<form class="example" action="/action_page.php" style="margin:5px;max-width:250px">
+		  <input type="text" placeholder="Search.." name="search2">
+		  <button type="submit"><i class="fa fa-search"></i></button>
+		</form>
+  </span>
+  <a href="javascript:void(0);" style="font-size:36px;" class="icon" onclick="myFunction()">&#8801;</a>
+</div>
+<!-- NAVBAR ENDS -->
+
+
+
+<!-- MAIN CONTAINER STARTS HERE -->
+
+<!-- SEARCH RESULTS GOES HERE -->
+<div class="wrapper">
+
+	<div class="Content">
+		<div class="videoResult">
+			<div class="video"> </div>
+			<div class="Title">Title</div>
+		</div>
+		
+		<div class="videoResult">
+			<div class="video"> </div>
+			<div class="Title">Title</div>
+		</div>
+		
+		<div class="videoResult">
+			<div class="video"> </div>
+			<div class="Title">Title</div>
+		</div>
+		
+		<div class="videoResult">
+			<div class="video"> </div>
+			<div class="Title">Title</div>
+		</div>
+		<div class="videoResult">
+			<div class="video"> </div>
+			<div class="Title">Title</div>
+		</div>
+		
+		<div class="videoResult">
+			<div class="video"> </div>
+			<div class="Title">Title</div>
+		</div>
+		
+		<div class="videoResult">
+			<div class="video"> </div>
+			<div class="Title">Title</div>
+		</div>
+		
+		<div class="videoResult">
+			<div class="video"> </div>
+			<div class="Title">Title</div>
+		</div>
+		<div class="videoResult">
+			<div class="video"> </div>
+			<div class="Title">Title</div>
+		</div>
+		
+		<div class="videoResult">
+			<div class="video"> </div>
+			<div class="Title">Title</div>
+		</div>
+		
+		<div class="videoResult">
+			<div class="video"> </div>
+			<div class="Title">Title</div>
+		</div>
+		
+		<div class="videoResult">
+			<div class="video"> </div>
+			<div class="Title">Title</div>
+		</div>
+		<div class="videoResult">
+			<div class="video"> </div>
+			<div class="Title">Title</div>
+		</div>
+		
+		<div class="videoResult">
+			<div class="video"> </div>
+			<div class="Title">Title</div>
+		</div>
+		
+		<div class="videoResult">
+			<div class="video"> </div>
+			<div class="Title">Title</div>
+		</div>
+		
+		<div class="videoResult">
+			<div class="video"> </div>
+			<div class="Title">Title</div>
+		</div>
+		<div class="videoResult">
+			<div class="video"> </div>
+			<div class="Title">Title</div>
+		</div>
+		
+		<div class="videoResult">
+			<div class="video"> </div>
+			<div class="Title">Title</div>
+		</div>
+		
+		<div class="videoResult">
+			<div class="video"> </div>
+			<div class="Title">Title</div>
+		</div>
+		
+		<div class="videoResult">
+			<div class="video"> </div>
+			<div class="Title">Title</div>
+		</div>
+	</div>
+	
+<!-- ADS GOES HERE -->
+	<div class="Ads">
+		
+		<span class="SearchBar">
+			<form class="example" action="/action_page.php">
+			  <input type="text" placeholder="Search.." name="search">
+			  <button type="submit"><i class="fa fa-search"></i></button>
+			</form>
+		</span>
+
+		<div class="GoogleAds" >
+			<p>GOOGLE ADS HERE</p>
+		</div>
+		
+		<div class="CustomAds">
+			<p>CUSTOM ADS HERE</p>
+		</div>
+	</div>
+</div>
+
+<!-- MAIN CONTAINER ENDS HERE -->
+
+
+
+
+
+
+<!-- FOOTER PART  -->
+<div class="footer">
+	<ul>
+	  <li><a style="pointer-events: none;">Designed By </a></li>
+	  <li><a href="http://innotrek.in/">&copy Innotrek Infocomm</a></li>
+	</ul>
+</div>	
+<!-- FOOTER PART ENDS -->
+
+
+
+<!-- JAVASCRIPTS GOES HERE -->
+
+<script>
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+</script>
+
+
+
+</body>
+</html>
